cdc676e65111dd1748efbcbe62b43bd4de9f6028
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-11-13 08:46:57 +0600

    Undefine commit

--- testproject/views.py
+++ testproject/views.py
@@ -1,17 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def test_view(request):
-    html = """
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <h2>This is a test page2</h2>
-        </body>
-        </html>
-    """
-    return HttpResponse(html)
+    return render(request, 'test.html')
+    # return HttpResponse(html)
